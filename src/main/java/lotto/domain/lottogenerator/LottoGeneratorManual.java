0e9431755f4a15902354138f07487bd8226377c1
ordinCode <ysh1346@naver.com>
2020-03-03 13:12:45 +0900

    style : 메소드 순서 변경

--- src/main/java/lotto/domain/lottogenerator/LottoGeneratorManual.java
+++ src/main/java/lotto/domain/lottogenerator/LottoGeneratorManual.java
@@ -12,6 +12,14 @@ import java.util.stream.Collectors;
 public class LottoGeneratorManual implements LottoGenerator {
 	private static final String ERROR_MESSAGE_NOT_INTEGER = "숫자가 아닌 문자를 입력하였습니다.";
 
+	@Override
+	public List<Lotto> generator(Customer customer) {
+		if (!customer.isUserLottoCountOverZero()) {
+			return new ArrayList<>();
+		}
+		return createManualLotto(customer.getManualLottoNumbers());
+	}
+
 	private static List<Lotto> createManualLotto(String[] manualLottoNumbers) {
 		List<Lotto> lottos = new ArrayList<>();
 		for (String numbers : manualLottoNumbers) {
@@ -31,13 +39,5 @@ public class LottoGeneratorManual implements LottoGenerator {
 		} catch (NumberFormatException e) {
 			throw new IllegalArgumentException(ERROR_MESSAGE_NOT_INTEGER);
 		}
-	}
-
-	@Override
-	public List<Lotto> generator(Customer customer) {
-		if (!customer.isUserLottoCountOverZero()) {
-			return new ArrayList<>();
-		}
-		return createManualLotto(customer.getManualLottoNumbers());
 	}
 }
